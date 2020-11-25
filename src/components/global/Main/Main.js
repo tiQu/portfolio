4413db74e8501c5edb63bf90319bf66f4e1fbc9c
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-11-25 10:51:23 -0800

    Added hexagon shape as building block

---
+++ src/components/global/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Main.scss';
+
+class Main extends React.Component{
+    render(){
+        return (
+            <main>
+                <div class="hexagon"><span>Hello</span></div>
+            </main>
+        );
+    }
+  }
+
+export default Main;
