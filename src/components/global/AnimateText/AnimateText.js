14679b7eb578b327437cbb59186a6ef86cc54651
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-12-14 09:55:49 -0800

    #9 Created basic rendering class

---
+++ src/components/global/AnimateText/AnimateText.js
@@ -0,0 +1,19 @@
+class AnimateText extends Component {
+    render(){
+      return(
+        <span aria-label={this.props.content}>
+            {this.props.copy.split("").map(function(c, ix){
+              let styles = {"animation-delay": (0.55 + ix / 10) + "s"}
+              return <span
+                aria-hidden="true"
+                key={ix}
+                style={styles}>
+                {c}
+              </span>;
+            })}
+          </span>
+      );
+    }
+  }
+
+export default AnimateText;
