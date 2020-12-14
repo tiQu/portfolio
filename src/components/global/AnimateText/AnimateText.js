4670e952b8ec2a64d16f49f43b3a491e0621632a
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-12-14 09:58:15 -0800

    #9 Added styles

--- src/components/global/AnimateText/AnimateText.js
+++ src/components/global/AnimateText/AnimateText.js
@@ -1,7 +1,7 @@
 class AnimateText extends Component {
     render(){
       return(
-        <span aria-label={this.props.content}>
+        <span className="animateThis" aria-label={this.props.content}>
             {this.props.copy.split("").map(function(c, ix){
               let styles = {"animation-delay": (0.55 + ix / 10) + "s"}
               return <span
