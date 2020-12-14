a4f9e16013fc19783a60c983727eced14188a6ac
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-12-13 18:17:10 -0800

    Adding router and professional component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './App.scss';
 import Main from './components/global/Main/Main';
 import Header from './components/global/Header/Header'
 import Footer from './components/global/Footer/Footer'
+import Home from './components/home/Home';
+import Professional from './components/professional/professional';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+        <Header />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/professional" component={Professional} />
+          </Switch>
+        </Router>
+        <Footer />
     </div>
   );
 }
---
+++ src/components/professional/Professional.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Professional extends React.Component{
+    render(){
+        return (
+            <section className="Professional">
+                Professional section
+            </section>
+        );
+    }
+  }
+
+export default Home;
