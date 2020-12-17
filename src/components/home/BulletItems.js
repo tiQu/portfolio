a63006df8cd9a7e3f2230b2bfd521468843bd32b
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-12-17 11:15:24 -0800

    Copy added to Home page

--- src/components/home/BulletItems.js
+++ src/components/home/BulletItems.js
@@ -1,43 +1,43 @@
 export const BulletItems = [
     {
-        bulletId: 'React',
-        bulletTitle: 'React',
-        bulletDescription: 'Something about React',
+        bulletId: 'CSharp',
+        bulletTitle: 'C#',
+        bulletDescription: 'years of experience in programming, testing, improving code in fast-paced work environments',
         bulletClasses: 'Home-bottom-section-left',
         bulletOffset: -100
     },
     {
-        bulletId: 'Angular',
-        bulletTitle: 'Angular',
-        bulletDescription: 'Something about Angular',
+        bulletId: 'NET',
+        bulletTitle: '.NET API',
+        bulletDescription: 'building endpoints with efficiency & frontend in mind',
         bulletClasses: 'Home-bottom-section-left',
         bulletOffset: -192
     },
     {
-        bulletId: 'NET',
-        bulletTitle: '.NET',
-        bulletDescription: 'Something about .NET',
+        bulletId: 'React',
+        bulletTitle: 'React',
+        bulletDescription: 'proficient in one of the leading frontend frameworks',
         bulletClasses: 'Home-bottom-section-left',
         bulletOffset: -284
     },
     {
         bulletId: 'SQL',
         bulletTitle: 'SQL',
-        bulletDescription: 'Something about SQL',
+        bulletDescription: 'astute thinker navigating complex database structures swiftly',
         bulletClasses: 'Home-bottom-section-right',
         bulletOffset: -100
     },
     {
         bulletId: 'R',
         bulletTitle: 'R',
-        bulletDescription: 'Something about R',
+        bulletDescription: 'stats-savvy developer solving complex data problems in an ever-growing community of R enthusiasts',
         bulletClasses: 'Home-bottom-section-right',
         bulletOffset: -192
     },
     {
         bulletId: 'dataAnalysis',
         bulletTitle: 'data analysis',
-        bulletDescription: 'Something about data analysis',
+        bulletDescription: 'long and proven track record for delivering insights from raw data, communicating meaningful findings clearly',
         bulletClasses: 'Home-bottom-section-right',
         bulletOffset: -284
     }
