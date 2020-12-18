332af706c4595c6f9ec258a73176ed0d2d580cdd
Tilman Queitsch <tqueitsch@my.bcit.ca>
2020-12-17 21:08:02 -0800

    #17 added mockups link in readme

--- src/components/home/BulletItems.js
+++ src/components/home/BulletItems.js
@@ -30,14 +30,14 @@ export const BulletItems = [
     {
         bulletId: 'R',
         bulletTitle: 'R',
-        bulletDescription: 'stats-savvy developer solving complex data problems in an ever-growing community of R enthusiasts',
+        bulletDescription: 'stats-savvy developer with a knack for solving complex data problems; contributing member of an ever-growing community of R enthusiasts',
         bulletClasses: 'Home-bottom-section-right',
         bulletOffset: -192
     },
     {
         bulletId: 'dataAnalysis',
         bulletTitle: 'data analysis',
-        bulletDescription: 'long and proven track record for delivering insights from raw data, communicating meaningful findings clearly',
+        bulletDescription: 'yearslong proven track record for delivering insights from raw data, communicating meaningful findings clearly',
         bulletClasses: 'Home-bottom-section-right',
         bulletOffset: -284
     }
