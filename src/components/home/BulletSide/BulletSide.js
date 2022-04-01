b724ca3c15d893efa5872a241edc13c09c2fe363
tilman queitsch <allrightyougotme@gmail.com>
2022-04-01 20:24:59 +0200

    convert to functional components; drop separator

--- src/components/home/BulletSide/BulletSide.js
+++ src/components/home/BulletSide/BulletSide.js
@@ -2,19 +2,15 @@ import React from 'react';
 import './BulletSide.scss';
 import { Link } from "react-scroll";
 
-class BulletSide extends React.Component{
-    render(){
-        return (
-            <Link activeClass="active" 
-            spy={true}
-            smooth={true}
-            offset={parseInt(this.props.offset)}
-            duration={500}
-            to={this.props.scrollToSection}>
-                <div className="bullet-text">{this.props.txt}</div>
-            </Link>
-        );
-    }
-  }
-
-export default BulletSide;
+export default function BulletSide() {
+    return (
+        <Link activeClass="active" 
+        spy={true}
+        smooth={true}
+        offset={parseInt(this.props.offset)}
+        duration={500}
+        to={this.props.scrollToSection}>
+            <div className="bullet-text">{this.props.txt}</div>
+        </Link>
+    );
+}
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,46 +7,42 @@ import BulletCentre from './BulletCentre/BulletCentre';
 // import FadeInSection from '../global/FadeInSection/FadeInSection';
 import { BulletItems } from './BulletItems.js'
 
-class Home extends React.Component{
-    render(){
-        return (
-            <section className="Home">
-                <div className="col-home-1">
-                    <div className="bullets-left">
-                        {BulletItems.slice(0,3).map((item) => {
-                                return (
-                                    <BulletSide scrollToSection={item.bulletId} txt={item.bulletTitle} offset={item.bulletOffset} />
-                                )
-                            })}
-                        {/* <BulletSide idx="b1" scrollToSection="b1Target" txt="React" /> */}
-                    </div>
-                </div>
-                <div className="col-home-2 Home-logo">
-                    <ParallaxProvider>
-                        <Hero />
-                    </ParallaxProvider>
-                    <div className="Home-bottom">
-                        {BulletItems.map((item) => {
+export default function Home() {
+    return (
+        <section className="Home">
+            <div className="col-home-1">
+                <div className="bullets-left">
+                    {BulletItems.slice(0,3).map((item) => {
                             return (
-                                <BulletCentre bulletClasses={item.bulletClasses} bulletId={item.bulletId} bulletTitle={item.bulletTitle} bulletDescription={item.bulletDescription} />
+                                <BulletSide scrollToSection={item.bulletId} txt={item.bulletTitle} offset={item.bulletOffset} />
                             )
                         })}
-                        {/* <SectionSeparator /> */}
-                        {/* <Element className="Home-bottom-section Home-bottom-section2" id="b6Target">Something about market research</Element> */}
-                    </div>
+                    {/* <BulletSide idx="b1" scrollToSection="b1Target" txt="React" /> */}
                 </div>
-                <div className="col-home-3">
-                    <div className="bullets-right">
-                        {BulletItems.slice(3,6).map((item) => {
-                                return (
-                                    <BulletSide scrollToSection={item.bulletId} txt={item.bulletTitle} offset={item.bulletOffset} />
-                                )
-                            })}
-                    </div>
+            </div>
+            <div className="col-home-2 Home-logo">
+                <ParallaxProvider>
+                    <Hero />
+                </ParallaxProvider>
+                <div className="Home-bottom">
+                    {BulletItems.map((item) => {
+                        return (
+                            <BulletCentre bulletClasses={item.bulletClasses} bulletId={item.bulletId} bulletTitle={item.bulletTitle} bulletDescription={item.bulletDescription} />
+                        )
+                    })}
+                    {/* <SectionSeparator /> */}
+                    {/* <Element className="Home-bottom-section Home-bottom-section2" id="b6Target">Something about market research</Element> */}
                 </div>
-            </section>
-        );
-    }
-  }
-
-export default Home;
+            </div>
+            <div className="col-home-3">
+                <div className="bullets-right">
+                    {BulletItems.slice(3,6).map((item) => {
+                            return (
+                                <BulletSide scrollToSection={item.bulletId} txt={item.bulletTitle} offset={item.bulletOffset} />
+                            )
+                        })}
+                </div>
+            </div>
+        </section>
+    );
+}
