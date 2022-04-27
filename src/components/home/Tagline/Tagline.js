b23be2ae86687116fda8490bf3f245af8fe80c77
tiqu <allrightyougotme@gmail.com>
2022-04-27 13:10:18 +0300

    remodeled home page

---
+++ src/components/home/Tagline/Tagline.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Tagline.scss';
+
+export default function Tagline() {
+  return (
+    <div className="tag-wrapper">
+        <div className="tag-item-wrapper">
+          <div className="svg-wrapper">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightbulb" viewBox="0 0 16 16">
+              <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z"/>
+            </svg>
+          </div>
+          <div className="tag">
+            Analytics developer with a proven track record for delivering relevant insights to global clients.
+          </div>
+        </div>
+        <div className="tag-item-wrapper">
+          <div className="svg-wrapper">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightning-charge-fill" viewBox="0 0 16 16">
+              <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
+            </svg>
+          </div>
+          <div className="tag">
+            Constantly curious R programmer with a knack for automating repetitive analysis.
+          </div>
+        </div>
+    </div >
+  );
+}
