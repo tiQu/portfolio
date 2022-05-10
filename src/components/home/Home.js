8f2ea852911bff559b985265a8e74be200bb9241
tiqu <allrightyougotme@gmail.com>
2022-05-10 20:57:24 +0300

    added animations; updated wording

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,6 +9,7 @@ import HomeDivider from './HomeDivider/HomeDivider';
 import svgConsolePlus from './HomeDivider/svgConsolePlus';
 import svgStack from './HomeDivider/svgStack';
 import svgGrowthChart from './HomeDivider/svgGrowthChart';
+import svgGear from './HomeDivider/svgGear';
 
 export default function Home() {
     return (
@@ -26,8 +27,10 @@ export default function Home() {
                     <li>&gt; In-company hackathon winner for biggest business impact</li>
                     <li>&gt; Shipped more than 200 projects as data analyst</li>
                 </ul>
-                <HomeDivider Graphic={svgStack} dividerText="Technologies I like" />
-                <div className="home-tech">React, SQL, R, AWS, Node.JS, Ruby on Rails, MongoDB, .NET APIs, Kotlin, Flutter, good ol' VBA</div>
+                <HomeDivider Graphic={svgStack} dividerText="Tech I use regularly" />
+                <div className="home-tech">React, SQL, R, AWS, Node.JS, Ruby on Rails, MongoDB, .NET APIs, Linux (Ubuntu, Manjaro, Fedora), good ol' VBA</div>
+                <HomeDivider Graphic={svgGear} dividerText="Also tinkering with" />
+                <div className="home-tech">Kotlin, Flutter, Lisp, Doom Emacs</div>
                 <HomeDivider Graphic={svgConsolePlus} dividerText="Apps I've worked on" />
                 <Projects />
             </div>
