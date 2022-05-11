8d6c92fe1f061775a7b76a365e8ff3e950f44fcf
tiqu <allrightyougotme@gmail.com>
2022-05-11 11:50:42 +0300

    added potatoes

--- src/components/home/Projects/ProjectItems.js
+++ src/components/home/Projects/ProjectItems.js
@@ -1,18 +1,26 @@
 export const ProjectItems = [
     {
         projectAsset: "assets/simple_stock_logo.png",
-        projectTitle: "Simple Stock",
+        projectTitle: "Simple Stock (2021)",
         projectSummary: "A nifty little mobile app for stock traders consuming data from an API, co-developed with three other programmers.",
         projectStack: "React-Native, Node.js, MongoDB",
-        projectDeployed: "See the code on GitHub.",
+        projectDeployed: "See the code and animated screencaps on GitHub.",
         projectUrl: "https://www.github.com/tiQu/simple-stock"
     },
     {
         projectAsset: "assets/amazombie_logo.png",
-        projectTitle: "Amazombie",
+        projectTitle: "Amazombie (2020)",
         projectSummary: "A tongue-in-cheek e-commerce site modeled after (but in no way related to) an online retailer. Group project with three other devs.",
         projectStack: "HTML5, CSS, Vanilla JS",
         projectDeployed: "Browse the website.",
         projectUrl: "https://fepamazombie.netlify.app/"
+    },
+    {
+        projectAsset: "assets/potato.png",
+        projectTitle: "Rotten Potatoes (2020)",
+        projectSummary: "Responsive potatoes! An API! Humble first steps in React after a few days of learning CSS and SCSS.",
+        projectStack: "React, HTML5, SCSS",
+        projectDeployed: "See the code and screenshots on GitHub.",
+        projectUrl: "https://www.github.com/tiQu/rotten-potatoes"
     }
 ]
