83d1af2f90874e29cc9339102ec8b76a4ab30132
tiqu <allrightyougotme@gmail.com>
2022-05-11 19:05:23 +0300

    layout updates

--- src/components/home/Tagline/Tagline.js
+++ src/components/home/Tagline/Tagline.js
@@ -4,26 +4,36 @@ import './Tagline.scss';
 export default function Tagline() {
   return (
     <div className="tag-wrapper">
-        <div className="tag-item-wrapper">
-          <div className="svg-wrapper">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightbulb-fill" viewBox="0 0 16 16">
-              <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5z"/>
-            </svg>
-          </div>
-          <div className="tag">
-            Analytics developer with a proven track record for delivering relevant insights to global clients.
-          </div>
+      <div className="tag-item-wrapper">
+        <div className="svg-wrapper">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-code" viewBox="0 0 16 16">
+          <path d="M5.854 4.854a.5.5 0 1 0-.708-.708l-3.5 3.5a.5.5 0 0 0 0 .708l3.5 3.5a.5.5 0 0 0 .708-.708L2.707 8l3.147-3.146zm4.292 0a.5.5 0 0 1 .708-.708l3.5 3.5a.5.5 0 0 1 0 .708l-3.5 3.5a.5.5 0 0 1-.708-.708L13.293 8l-3.147-3.146z"/>
+        </svg>
         </div>
-        <div className="tag-item-wrapper">
-          <div className="svg-wrapper">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightning-charge-fill" viewBox="0 0 16 16">
-              <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
-            </svg>
-          </div>
-          <div className="tag">
-            Constantly curious R programmer with a knack for automating repetitive analyses.
-          </div>
+        <div className="tag">
+          Certified web programmer with extensive professional experience.
         </div>
+      </div> 
+      <div className="tag-item-wrapper">
+        <div className="svg-wrapper">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightbulb-fill" viewBox="0 0 16 16">
+            <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5z"/>
+          </svg>
+        </div>
+        <div className="tag">
+          Analytics developer with a proven track record for delivering relevant insights to global clients.
+        </div>
+      </div>
+      <div className="tag-item-wrapper">
+        <div className="svg-wrapper">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightning-charge-fill" viewBox="0 0 16 16">
+            <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
+          </svg>
+        </div>
+        <div className="tag">
+          Constantly curious R nerd with a knack for automating repetitive analyses.
+        </div>
+      </div>
     </div >
   );
 }
