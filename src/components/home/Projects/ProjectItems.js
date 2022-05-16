e3ef0e51950ad1815b44174006360c610c68125c
tiqu <allrightyougotme@gmail.com>
2022-05-16 10:02:29 +0300

    updated layout for footer; updated logo; wording updates

--- src/components/home/Projects/ProjectItems.js
+++ src/components/home/Projects/ProjectItems.js
@@ -2,7 +2,7 @@ export const ProjectItems = [
     {
         projectAsset: "assets/simple_stock_logo.png",
         projectTitle: "Simple Stock (2021)",
-        projectSummary: "A nifty little mobile app for stock traders consuming data from an API, co-developed with three other programmers.",
+        projectSummary: "A nifty little mobile app for stock traders consuming data from an API.",
         projectStack: "React-Native, Node.js, MongoDB",
         projectDeployed: "See the code and animated screencaps on GitHub.",
         projectUrl: "https://www.github.com/tiQu/simple-stock"
@@ -10,7 +10,7 @@ export const ProjectItems = [
     {
         projectAsset: "assets/amazombie_logo.png",
         projectTitle: "Amazombie (2020)",
-        projectSummary: "A tongue-in-cheek e-commerce site modeled after (but in no way related to) an online retailer. Group project with three other devs.",
+        projectSummary: "A tongue-in-cheek e-commerce site modeled after (but in no way related to) an online retailer.",
         projectStack: "HTML5, CSS, Vanilla JS",
         projectDeployed: "Browse the website.",
         projectUrl: "https://fepamazombie.netlify.app/"
