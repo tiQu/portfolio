a154c8c19e50243015bbfb3ec666227a28cad403
allrightyougotme@gmail.com <allrightyougotme@gmail.com>
2022-11-04 20:15:56 -0700

    added copy for new position

--- src/components/professional/Professional.js
+++ src/components/professional/Professional.js
@@ -8,17 +8,24 @@ export default function Professional() {
         <section className="professional">
             <Card staticChar={CardItems[0].cardStaticChar}
                     animateChar={CardItems[0].cardAnimateChar}
-                    addCharClass="blink"
+                    addCharClass="hop"
                     cardHeader={CardItems[0].cardHeader}
                     cardSummary={CardItems[0].cardSummary}
                     cardList={CardItems[0].cardList}
             />
             <Card staticChar={CardItems[1].cardStaticChar}
                     animateChar={CardItems[1].cardAnimateChar}
-                    addCharClass="spacing"
+                    addCharClass="blink"
                     cardHeader={CardItems[1].cardHeader}
                     cardSummary={CardItems[1].cardSummary}
                     cardList={CardItems[1].cardList}
+            />
+            <Card staticChar={CardItems[2].cardStaticChar}
+                    animateChar={CardItems[2].cardAnimateChar}
+                    addCharClass="spacing"
+                    cardHeader={CardItems[2].cardHeader}
+                    cardSummary={CardItems[2].cardSummary}
+                    cardList={CardItems[2].cardList}
             />
         </section>
     );
